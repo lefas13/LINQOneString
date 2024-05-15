c3e05fc59c17eaf567f6b9028ff101e3d34dadcf
User <User@WIN-J9S0JJK5DN4>
2024-05-15 04:24:26 +0300

    Создано приложение с использованием LINQ.

---
+++ LINQOneString/LINQString.cs
@@ -0,0 +1,7 @@
+﻿string filePath = "C:\\Users\\User\\Desktop\\учеба\\ООПиП\\лаб9\\number.txt";
+double sumOfEvenNumbers = File.ReadAllText(filePath)
+    .Split(' ')
+    .Select(double.Parse)
+    .Where(num => num % 2 == 0)
+    .Sum();
+Console.WriteLine($"Сумма чётных чисел равна {sumOfEvenNumbers}");
