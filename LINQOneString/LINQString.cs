fb8ddac16856d3f41d400f462c1c07b5abd19857
User <User@WIN-J9S0JJK5DN4>
2024-05-15 04:34:08 +0300

    Добавлены обработчики исключений.

--- LINQOneString/LINQString.cs
+++ LINQOneString/LINQString.cs
@@ -1,7 +1,23 @@
-﻿string filePath = "C:\\Users\\User\\Desktop\\учеба\\ООПиП\\лаб9\\number.txt";
-double sumOfEvenNumbers = File.ReadAllText(filePath)
-    .Split(' ')
-    .Select(double.Parse)
-    .Where(num => num % 2 == 0)
-    .Sum();
-Console.WriteLine($"Сумма чётных чисел равна {sumOfEvenNumbers}");
+﻿class LINQString
+{
+    static void Main(string[] args)
+    {
+        try
+        { 
+            Console.WriteLine($"Сумма чётных чисел равна {File.ReadAllText("C:\\Users\\User\\Desktop\\учеба\\ООПиП\\лаб9\\number.txt").Split(' ').Select(double.Parse).Where(num => num % 2 == 0).Sum()}");
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("Файл не найден.");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Некорректный формат данных в файле.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
+        Console.ReadLine();
+    }
+}
